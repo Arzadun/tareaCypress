eb47df7d5089a70f2b78e461d0cb4e5a4dfae512
sebas <sebastianarzadun@gmail.com>
2023-01-16 01:29:23 -0300

    Added customCommands and new fixture file

--- cypress/e2e/LoginAutomationExercise.cy.js
+++ cypress/e2e/LoginAutomationExercise.cy.js
@@ -1,7 +1,45 @@
 /// <reference types="cypress" />
 
-describe('Login functionality', () => {
+/*describe('Login functionality', () => {
   
+  it('User is in required page', () => {
+    cy.visit("https://automationexercise.com/");
+
+    cy.url().should('eq', 'https://automationexercise.com/')
+
+  })
+
+  it('User is able to navigate to login page', () => {
+
+    cy.visit("https://automationexercise.com/");
+
+    cy.fixture("home").then((home)=>{
+      cy.get(home.loginPageButton).click();
+    })
+
+    cy.fixture("login").then((login)=>{
+      cy.get(login.loginFormHeader).should("be.visible");
+    })
+ 
+  })
+
+  it('User is able to enter his credentials', () => {
+
+    cy.visit("https://automationexercise.com/login");
+
+    cy.loginIntoApplication("sebatest@mailinator.com", "12345")
+
+    cy.fixture("mainUser").then((mainUser)=>{
+     cy.get(mainUser.logoutButton).should("be.visible");
+    })
+
+    
+  })
+
+})
+*/
+describe('Login functionality negative scenario', () => {
+
   it('User is in required page', () => {
     cy.visit("https://automationexercise.com/");
 
@@ -22,56 +60,15 @@ describe('Login functionality', () => {
     })
 
   })
-/*
-  it('User is able to enter his credentials', () => {
+
+  it('User is nit able to login into the page', () => {
 
     cy.visit("https://automationexercise.com/login");
 
-    LoginPage.enterCredentials("sebatest@mailinator.com", "12345")
-
-    cy.get(LoginPage.emailInput).should("have.value" , "sebatest@mailinator.com")
-
-    cy.get(LoginPage.passwordInput).should("have.value" , "12345")
-
-    LoginPage.submitLogin();
-
-    cy.get('.shop-menu > .nav > :nth-child(4) > a').should("have.text" , " Logout");
+    cy.loginIntoApplication("sebatest@mailinator.com", "1234")
+    
+    cy.fixture("login").then((login)=>{
+    cy.get(login.incorrectCredentialsText).should("have.text" , "Your email or password is incorrect!");
+    }) 
   })
-
-})
-
-describe('Login functionality negative scenario', () => {
-
-  it('User is in required page', () => {
-    cy.visit("https://automationexercise.com/");
-
-    cy.url().should('eq', 'https://automationexercise.com/')
-
-  })
-
-  it('User is able to navigate to login page', () => {
-
-    cy.visit("https://automationexercise.com/");
-
-    HomePage.navigateToLoginPage();
-
-    cy.get('.login-form > h2').should("be.visible");
-
-  })
-
-  it('User is able to login into the page', () => {
-
-    cy.visit("https://automationexercise.com/login");
-
-    LoginPage.enterCredentials("sebatest@mailinator.com", "1234")
-
-    cy.get(LoginPage.emailInput).should("have.value" , "sebatest@mailinator.com")
-
-    cy.get(LoginPage.passwordInput).should("have.value" , "1234")
-
-    LoginPage.submitLogin();
-
-    cy.get('.login-form > form > p').should("have.text" , "Your email or password is incorrect!");
-
-  })*/
 })
